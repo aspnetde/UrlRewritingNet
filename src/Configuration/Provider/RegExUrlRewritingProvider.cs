ee51a27f7e92e8e0079c4509cdd96f08d84a8eaa
edr <fuffa@deltatre.com>
2015-10-30 11:43:07 +0100

    Added the name on RegExUrlRewritingProvider to avoid errors on IIS7

--- src/Configuration/Provider/RegExUrlRewritingProvider.cs
+++ src/Configuration/Provider/RegExUrlRewritingProvider.cs
@@ -4,6 +4,13 @@ namespace UrlRewritingNet.Configuration.Provider
 {
     public class RegExUrlRewritingProvider : UrlRewritingProvider
     {
+        public override string Name
+        {
+            get
+            {
+                return "RegEx";
+            }
+        }
         public override RewriteRule CreateRewriteRule()
         {
             return new RegExRewriteRule();
