c5c929d6f63b35f0d3c67d115f84387d4c0eb7c8
Sebastiaan Janssen <sebastiaan@umbraco.com>
2016-01-06 16:57:59 +0100

    Makes UrlRewritingProviderCollection thread safe

--- src/Configuration/Provider/UrlRewritingProviderCollection.cs
+++ src/Configuration/Provider/UrlRewritingProviderCollection.cs
@@ -1,32 +1,55 @@
 using System;
 using System.Configuration.Provider;
-
+using System.Threading;
 namespace UrlRewritingNet.Configuration.Provider
 {
     public class UrlRewritingProviderCollection : ProviderCollection
     {
+        private object lockObject = new object();
+        public new UrlRewritingProvider this[string name]
+        {
+            get
+            {
+                return (UrlRewritingProvider)base[name];
+            }
+        }
         public override void Add(ProviderBase provider)
         {
             if (provider == null)
+            {
                 throw new ArgumentNullException("provider");
-
+            }
             if (!(provider is UrlRewritingProvider))
             {
-                string msg = string.Format("Provider must implement type {0}", typeof(UrlRewritingProvider));
-                throw new ArgumentException(msg, "provider");
+                string message = string.Format("Provider must implement type {0}", typeof(UrlRewritingProvider).ToString());
+                throw new ArgumentException(message, "provider");
             }
-
-            base.Add(provider);
+            object obj;
+            Monitor.Enter(obj = this.lockObject);
+            try
+            {
+                if (base[provider.Name] == null)
+                {
+                    base.Add(provider);
+                }
+            }
+            finally
+            {
+                Monitor.Exit(obj);
+            }
         }
-
         public void CopyTo(UrlRewritingProvider[] providers, int index)
         {
-            base.CopyTo(providers, index);
-        }
-
-        new public UrlRewritingProvider this[string name]
-        {
-            get { return (UrlRewritingProvider)base[name]; }
+            object obj;
+            Monitor.Enter(obj = this.lockObject);
+            try
+            {
+                base.CopyTo(providers, index);
+            }
+            finally
+            {
+                Monitor.Exit(obj);
+            }
         }
     }
 }
